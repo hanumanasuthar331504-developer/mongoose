81e8b72295549b0a95786f755670db2f025bafbd
hanumansuthar331504-developer <hanumansuthar331504@gmail.com>
2026-01-13 14:15:23 +0530

    make post api

--- index.js
+++ index.js
@@ -5,12 +5,33 @@
 
    const app = express();
 
+   app.use(express.json());
    await mongoose.connect("mongodb://localhost:27017/data").then(()=>{
    console.log("_________connected_________");
    })
    app.get('/',async (req,res)=>{
       const studentData = await studentModel.find()
       res.send(studentData)
+   })
+
+   app.post('/save',async(req,res)=>{
+      console.log(req.body)
+      const {name,age,email}=req.body
+      if(!req.body || !name || !age || !email){
+         res.send({
+         message:"data not stored",
+         success:false,
+         storedInfo:null
+      });
+      return false;
+      }
+      const studentData=await studentModel.create(req.body)
+
+      res.send({
+         message:"data stored",
+         success:true,
+         storedInfo:studentData
+      })
    })
 
    app.listen(3200)
